43f9e1bd406184ef4fa7e6f819669058df4e148b
gigery33 <niputinxd@gmail.com>
2026-05-17 21:14:42 +0300

    added visalisation of shelters

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import geolocationReducer from "./geolocation";
 import authReducer from "./auth";
+import sheltersReducer from "./shelters";
 
 export const store = configureStore({
   reducer: {
     geolocation: geolocationReducer,
     auth: authReducer,
+    shelters: sheltersReducer,
   },
 });
 
